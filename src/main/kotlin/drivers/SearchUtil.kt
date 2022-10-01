faec8b7ef77b00732f8b2396651ad4860ebdaff5
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-10-01 22:12:19 +0800

    Refactor valid board state fn; Add timer util

--- src/main/kotlin/drivers/SearchUtil.kt
+++ src/main/kotlin/drivers/SearchUtil.kt
@@ -3,6 +3,7 @@ package drivers
 import structs.Board
 import structs.BoardItem
 import structs.BoardItemPiece
+import structs.WindowedTimer
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.random.Random
 
@@ -35,11 +36,7 @@ object SearchUtil {
     }
 
     fun randomSearchSolution(board: Board, depth: Int): Board? {
-        if (!BoardUtil.isBoardValid(board)) {
-            invalids.incrementAndGet()
-            return null
-        }
-        if (!BoardUtil.isBoardValid2(board)) {
+        if (!BoardUtil.isBoardValid3(board)) {
             invalids.incrementAndGet()
             return null
         }
@@ -50,7 +47,9 @@ object SearchUtil {
 
         if (snapshot.isEmpty()) return board
 
-        return snapshot.mapAndFindNonNull { recursiveSearchSolution(board, it.second, depth) }
+        return snapshot.mapAndFindNonNull { recursiveSearchSolution(board, it.second, depth) }.apply {
+            TimerUtil.stamp(windowedTimer)
+        }
     }
 
     private val random = Random(0)
@@ -58,14 +57,16 @@ object SearchUtil {
     private val atomicLong by lazy { AtomicLong(0) }
     private val undos by lazy { AtomicLong(0) }
     private val invalids by lazy { AtomicLong(0) }
+    private val windowedTimer by lazy { WindowedTimer() }
 
     private fun printSearchBoard(board: Board, depth: Int) {
         val iterations = atomicLong.getAndIncrement()
         print("\u001B[H\u001B[2J")
         System.out.flush()
         println("Skipped: $invalids, Undos: $undos")
-        println("Search Depth: $depth, iterations: $iterations")
+        println("Search Depth: $depth, iterations: $iterations, Average per iteration: ${"%.2f".format(windowedTimer.averageCalled)}ms")
         BoardUtil.printBoard(board)
+        println()
     }
 
 }
